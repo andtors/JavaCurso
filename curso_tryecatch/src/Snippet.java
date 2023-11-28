66d2b6e4afedda495cf2c37d40ecfa23b5d23a59
andtors <andre.tors448@gmail.com>
2023-11-28 16:20:53 -0300

    excluido arquivo errado

--- curso_tryecatch/src/Snippet.java
+++
@@ -1,8 +0,0 @@
-
-
-public class Snippet {
-	É um bloco que contém código a ser executado independentemente de ter 
-	ocorrido ou não uma exceção. 
-	• Exemplo clássico: fechar um arquivo, conexão de banco de dados, ou outro 
-	recurso específico ao final do processamento
-}
